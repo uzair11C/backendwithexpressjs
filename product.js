5a12cfa530c30c7e6d9c06f49b9827f68dcb4c6f
Uzair Abdullah <uzairabdullah973@gmail.com>
2021-12-24 10:08:46 +0500

    initial commit

---
+++ product.js
@@ -0,0 +1,39 @@
+// class Product
+// {
+//     constructor(ProductID,Name,Type,Price,Quantity)
+//     {
+//         this.ProductID = ProductID
+//         this.Name = Name
+//         this.Type = Type
+//         this.Price = Price
+//         this.Quantity = Quantity
+//     }
+// }
+
+// class User
+// {
+//     constructor(ID,FirstName,LastName,Country,City,County,ContactNum,Email)
+//     {
+//         this.ID = ID
+//         this.FirstName = FirstName
+//         this.LastName = LastName
+//         this.Country = Country
+//         this.City = City
+//         this.County = County
+//         this.ContactNum = ContactNum
+//         this.Email = Email
+//     }
+// }
+
+class product
+{
+    constructor(ID,Name)
+    {
+        this.ID = ID
+        this.Name = Name
+    }
+}
+
+module.exports = product
+    // Product,
+    // User,
